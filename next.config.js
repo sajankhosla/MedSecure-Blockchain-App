f94134ec2f3be7a64dfd7b5ee1e9df9a9d30a46f
Khosla <sajan.khosla@gmail.com>
2025-04-11 09:36:11 +0100

    Add next.config.js and .npmrc for better Vercel compatibility

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  reactStrictMode: true,
+  transpilePackages: [
+    'react-native',
+    'react-native-web',
+    'expo',
+    'expo-router',
+    '@expo/vector-icons',
+    'react-native-gesture-handler',
+    'react-native-reanimated',
+    'react-native-safe-area-context',
+    'react-native-screens',
+  ],
+}; 
